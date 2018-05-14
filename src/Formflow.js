b16e5debe020e952b67953847e527173a8f11264
chentao <3413218@gmail.com>
2018-05-14 23:19:08 +0800

    对接了 jquery plugin 的 api

--- src/Formflow.js
+++ src/Formflow.js
@@ -4,26 +4,82 @@ var uuid = require("uuid");
 var $ = require("jquery")
 var Logger = require("js-logger")
 Logger.useDefaults();
-Logger.info("OMG! Check  out!");
+Logger.setLevel(Logger.DEBUG)
+// Logger.info("OMG! Check  out!");
 
 const Formflow = (($)=>{
     class Formflow {
-        constructor(){
+        constructor(element,options) {
+            this._element = element;
+            this.options = _.extend({},options,Formflow.defaultOptions)
+            Logger.debug("merged options:",this.options)
             this._init();
         }
 
-        set_w(width){
+        set_w(width) {
 
         }
 
-        _init(){
+        _init() {
+            this.data = [];
 
         }
 
-        setData(){
+        _render() {
+            const $element = this._element, data = this.data,options = this.options;
+            let _data = this._calcRowData(data,options.layout),trs = "";
+            for(let i = 0; i < _data.length; i++){
+                let tr = "<tr>";
+                let rowData = _data[i],tds = "";
+                for(let j = 0; j < rowData.length; j++){
+                    tds += `<td class="hc-formflow-label">${rowData[j].kText}</td><td class="hc-formflow-value">${rowData[j].vText}</td>`
+                }
+                tr = `<tr>`+
+                        tds +
+                    `</tr>`
+                trs += tr
+            }
+            let tableDom = `<table class="hc-formflow">`+
+                                `<tbody>`+
+                                        trs+
+                                `</tbody>`+
+                            `</table>`
+            $element.empty().append(tableDom)
+
+        }
+
+        /**
+         * calculate pos for every single data
+         * @param data
+         * @returns {Array}
+         * @private
+         */
+        _calcRowData(data,layout) {
+            let maxColNum = layout[0]
+            layout.forEach((colNum) => {
+                if(colNum > maxColNum)
+                    maxColNum = colNum;
+            })
+            let i = 0,row = [],rowedData = [];
+            data.forEach((col) => {
+                if(i === 0 || i == maxColNum){
+                    rowedData.push(row = [])
+                }
+                i++
+                row.push(col)
+            })
+            Logger.debug("rowedData:",rowedData)
+            return rowedData
+        }
+
+        setData(data){
+            Logger.debug("invoke setData function")
             data.forEach(function(val){
                 _.extend(val,Formflow.defaultMetaOptions())
             })
+            this.data = data
+            Logger.debug("merged data:",data)
+            this._render();
         }
 
         static defaultMetaOptions(){
@@ -35,12 +91,21 @@ const Formflow = (($)=>{
         }
 
         static _jQueryInterface(config){
+            const _config = typeof config === 'object' ? config : null
+            const params = Array.prototype.slice.call(arguments,1)
+            const methodName = arguments[0]
             return this.each(function(){
                 const $element = $(this);
                 let data = $element.data("hc-formflow")
                 if(!data){
-                    data = new Formflow()
+                    data = new Formflow($element,_config)
                     $element.data("hc-formflow",data);
+                }
+                if(typeof methodName === 'string'){
+                    if(data[config] === 'undefined'){
+                        throw new TypeError(`No method "${config}"`)
+                    }
+                    data[methodName].apply(data,params)
                 }
             })
         }
@@ -60,12 +125,12 @@ const Formflow = (($)=>{
         layout:[1,2,4] // layout cols contains 1,2,4 three
     }
 
-    $.fn.formflow = Formflow._jQueryInterface();
+    $.fn.formflow = Formflow._jQueryInterface;
     $.fn.formflow.Constructor = Formflow
 
 })($)
-let test = new Formflow()
-test.setData(data)
+$("#form").formflow();
+$("#form").formflow("setData",data)
 module.exports = Formflow;
 
 
