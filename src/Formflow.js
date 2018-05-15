b2c3fe5f98fe76c6dfd9ea322884c9a31301e0b7
abc3660170 <3413218@qq.com>
2018-05-15 20:19:13 +0800

    1.支持index排序 2.支持weight字段

--- src/Formflow.js
+++ src/Formflow.js
@@ -8,6 +8,8 @@ Logger.setLevel(Logger.DEBUG)
 // Logger.info("OMG! Check  out!");
 
 const Formflow = (($)=>{
+    const NAME = 'formflow'
+    const DATA_KEY = 'hc.'
     class Formflow {
         constructor(element,options) {
             this._element = element;
@@ -32,7 +34,11 @@ const Formflow = (($)=>{
                 let tr = "<tr>";
                 let rowData = _data[i],tds = "";
                 for(let j = 0; j < rowData.length; j++){
-                    tds += `<td class="hc-formflow-label">${rowData[j].kText}</td><td class="hc-formflow-value">${rowData[j].vText}</td>`
+                    if(typeof rowData[j].vFill !== 'undefined'){
+                        tds += `<td class="hc-formflow-value"  colspan="${rowData[j].colspan}">${rowData[j].vFill}</td>`
+                    }else{
+                        tds += `<td class="hc-formflow-label" colspan="${rowData[j].kColspan}">${rowData[j].kText}</td><td class="hc-formflow-value"  colspan="${rowData[j].vColspan}">${rowData[j].vText}</td>`
+                    }
                 }
                 tr = `<tr>`+
                         tds +
@@ -55,38 +61,91 @@ const Formflow = (($)=>{
          * @private
          */
         _calcRowData(data,layout) {
-            let maxColNum = layout[0]
-            layout.forEach((colNum) => {
-                if(colNum > maxColNum)
-                    maxColNum = colNum;
-            })
+            /*** first base max number of layout fit rows  ***/
+            let maxColNum = layout;
             let i = 0,row = [],rowedData = [];
             data.forEach((col) => {
-                if(i === 0 || i == maxColNum){
+                // current col need space colspan
+                const colspan = col.weight ? col.weight : 1;
+                // the rest of current row's space
+                const remainedCol = maxColNum - i;
+                // if fill all columns or comes a long col which weight > rest of space
+                if(i % maxColNum === 0 || colspan > remainedCol){
                     rowedData.push(row = [])
+                    i = colspan
+                }else{
+                    i = i + colspan
                 }
-                i++
+                col.vColspan = colspan * 2  - 1;
                 row.push(col)
             })
-            Logger.debug("rowedData:",rowedData)
-            return rowedData
+
+            /*** fill evey rows rest space with blanks ***/
+            return this._fillBlanks(rowedData,this.options.layout)
+        }
+
+        _fillBlanks(rowsData,layout) {
+            let _rowsData = $.extend([],rowsData),self = this;
+            _rowsData.forEach(function(row){
+                var emptyColNum = layout * 2;
+                row.forEach((col) => {
+                    emptyColNum = emptyColNum - col.kColspan - col.vColspan
+                })
+                if(emptyColNum > 0)
+                    row.push(self._formatColData({"vFill":"","colspan":emptyColNum}))
+            })
+            Logger.debug("filledBlanks data is:",_rowsData)
+            return _rowsData;
+        }
+
+        _formatColData(data) {
+           data = $.extend({},Formflow.defaultMetaOptions(),data)
+           return data
         }
 
         setData(data){
+            let self = this;
             Logger.debug("invoke setData function")
-            data.forEach(function(val){
-                _.extend(val,Formflow.defaultMetaOptions())
+
+            // sort data
+            //self.sortDatabyIndex(data)
+            Logger.debug("sortbyIndex data:",data)
+
+            // format data
+            data.forEach(function(val,index){
+                data[index] = self._formatColData(val)
             })
+
             this.data = data
             Logger.debug("merged data:",data)
             this._render();
+        }
+
+        sortDatabyIndex(data) {
+            if(!$.isArray(data) || data.length === 0)
+                return [];
+            if(typeof data[0].index !== 'undefined'){
+                // if data has index property,sort data by index
+                data.sort(function(a,b){
+                    if(a.index > b.index){
+                        return 1
+                    }
+                    if(a.index < b.index){
+                        return -1
+                    }
+                    return 0
+                })
+            }
         }
 
         static defaultMetaOptions(){
             return {
                 weight:Formflow.NORMAL,
                 id:uuid(),
-                index:Formflow.INDEXSTART
+                index:Formflow.INDEXSTART,
+                originOrder:Formflow.INDEXSTART++,
+                kColspan:1,
+                vColspan:1
             }
         }
 
@@ -111,22 +170,25 @@ const Formflow = (($)=>{
         }
     }
 
-// weight var
-    Formflow.LIGHTER = -2
-    Formflow.LIGHT = -1
-    Formflow.NORMAL = 0
-    Formflow.HEAVEY = 1
-    Formflow.HEAVEYER = 2
+
 
 // the index of index and groupstart
     Formflow.INDEXSTART = 0
 
     Formflow.defaultOptions = {
-        layout:[1,2,4] // layout cols contains 1,2,4 three
+        layout:5 // layout cols contains 1,2,4 three
     }
 
-    $.fn.formflow = Formflow._jQueryInterface;
-    $.fn.formflow.Constructor = Formflow
+    let plugin = $.fn[NAME] = Formflow._jQueryInterface;
+                 $.fn[NAME].Constructor = Formflow
+
+    // weight var
+    // todo 静态变量暴露方式不优雅
+    plugin.LIGHTER = Formflow.LIGHTER = -2
+    plugin.LIGHT = Formflow.LIGHT = -1
+    plugin.NORMAL = Formflow.NORMAL = 0
+    plugin.HEAVEY = Formflow.HEAVEY = 1
+    plugin.HEAVEYER = Formflow.HEAVEYER = 2
 
 })($)
 $("#form").formflow();
